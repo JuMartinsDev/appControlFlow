aa83344438c0fe9a4459d3031acb700c75ea7211
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-16 21:22:06 -0300

    atualização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ControlFlowComponent } from './components/control-flow/control-flow.component';
 import { InterpolacaoComponent } from './components/interpolacao/interpolacao.component';
+import { PropertieBindingComponent } from './components/propertie-binding/propertie-binding.component';
+import { DiretivaComponent } from './components/diretiva/diretiva.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,
-    InterpolacaoComponent, ControlFlowComponent],
+  imports: [
+    RouterOutlet,
+    //InterpolacaoComponent,
+    //ControlFlowComponent,
+    PropertieBindingComponent,
+    DiretivaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
